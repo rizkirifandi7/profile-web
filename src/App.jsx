69a8db186d992ba0c67dd26e00c26803b01f2db7
rizkirifandi7 <rizkirifandi7@gmail.com>
2024-04-22 11:33:41 +0700

    feat: update folder name & serve content

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { DarkModeContext } from "./hooks/useDarkModeContext";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import Header from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Qualification from "./components/qualification/Qualification";
 import Service from "./components/serve/Service";
